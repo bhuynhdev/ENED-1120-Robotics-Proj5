1b8fd59901ba44d7278533606f6e880af2237c60
baohuynhlam <shinsusenju@aol.com>
2020-06-01 21:13:10 +0700

    Create 2 main programs for demonstration purpose

--- Codes/Simulation/main2.py
+++ Codes/Simulation/main2.py
@@ -1,14 +1,15 @@
 """
-Main program 1 to run ENED simulation
-Robot traverse entire area without picking up box
+Main program 2 to run ENED simulation
+Robot traverses area and knows to pick the right box
 while also avoid obstacles and update IPS info
 """
 
 from myconstants import BARCODE, HOME
 from mysimulator import Simulator
 
+
 if __name__ == "__main__":
-    target_barode = [1, 1, 1, 1]  # Invalid barcode
+    target_barode = BARCODE[1]
     start_pos = HOME[0]
 
     game = Simulator(start_pos, target_barode)
